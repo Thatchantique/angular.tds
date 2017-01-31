1a4498f049b5606684654b9f07d51dd5da7fbb5d
Guillaume GAUJAC <ggaujacdu14@gmail.com>
2017-01-31 15:23:11 +0100

    Ajout services, links, scripts, directives.

--- td1/Ex2/app/ServicesApp.js
+++ td1/Ex2/app/ServicesApp.js
@@ -1,4 +1,4 @@
 /**
  * Created by gaujac on 31/01/17.
  */
-var ex2App = angular.module('',[]);
+var ex2App = angular.module('ex2App',["$http", "$cookies"]);
--- td1/Ex2/app/ServicesController.js
+++ td1/Ex2/app/ServicesController.js
@@ -1,11 +1,29 @@
 /**
  * Created by gaujac on 31/01/17.
  */
-ex2App.constructor('ex2App', function () {
+ex2App.constructor('ex2App', ["$http","$cookies",function () {
     var self = this;
 
-    var services;
+    this.services= [
+        {
+            "name": "Web Development",
+            "price": 300,
+            "active":true
+        },{
+            "name": "Design",
+            "price": 400,
+            "active":false
+        },{
+            "name": "Integration",
+            "price": 250,
+            "active":false
+        },{
+            "name": "Formation",
+            "price": 220,
+            "active":false
+        }];
 
+    services_select;
 
     this.total = function () {
 

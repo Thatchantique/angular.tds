8ddb393204099835f333573ddaa72a11c1698ffa
Guillaume Gaujac <ggaujacpro@gmail.com>
2017-02-14 09:48:05 +0100

    Ajout du TD2

---
+++ td2/ex1/app/listeApp.js
@@ -0,0 +1,4 @@
+/**
+ * Created by Gaujac on 07/02/2017.
+ */
+var listApp1 = angular.module('listApp1',["ngCookies"]);
---
+++ td2/ex1/index.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gaujac
+ * Date: 31/01/17
+ * Time: 14:28
+ */
+
+require "exercice1.html";
+
+?>

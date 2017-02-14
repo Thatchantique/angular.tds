fd39a3bc83470c5ff277bb77316f8d2d7a32af43
Guillaume Gaujac <ggaujacpro@gmail.com>
2017-02-14 10:44:29 +0100

    Controller finis et ajout du json

--- td2/ex1/app/listeApp.js
+++ td2/ex1/app/listeApp.js
@@ -1,4 +1,4 @@
 /**
  * Created by Gaujac on 07/02/2017.
  */
-var listApp1 = angular.module('listApp1',["ngCookies"]);
+var listApp1 = angular.module('listApp1',[]);
--- td2/ex1/app/listeController.js
+++ td2/ex1/app/listeController.js
@@ -1,77 +1,78 @@
 /**
  * Created by Gaujac on 07/02/2017.
  */
-listApp1.controller('listAppController', ["$http","$cookies",function ($http) {
+listApp1.controller('listAppController', ["$http", function ($http) {
 
 
     var self = this;
-    this.dispoItems =
-        [
-            {
-                "url": "http://tutorialzine.com/2013/07/50-must-have-plugins-for-extending-twitter-bootstrap/",
-                "title": "50 Must-have plugins for extending Twitter Bootstrap",
-                "image": "http://cdn.tutorialzine.com/wp-content/uploads/2013/07/featured_4-100x100.jpg"
-            },
-            {
-                "url": "http://tutorialzine.com/2013/08/simple-registration-system-php-mysql/",
-                "title": "Making a Super Simple Registration System With PHP and MySQL",
-                "image": "http://cdn.tutorialzine.com/wp-content/uploads/2013/08/simple_registration_system-100x100.jpg"
-            },
-            {
-                "url": "http://tutorialzine.com/2013/08/slideout-footer-css/",
-                "title": "Create a slide-out footer with this neat z-index trick",
-                "image": "http://cdn.tutorialzine.com/wp-content/uploads/2013/08/slide-out-footer-100x100.jpg"
-            },
-            {
-                "url": "http://tutorialzine.com/2013/06/digital-clock/",
-                "title": "How to Make a Digital Clock with jQuery and CSS3",
-                "image": "http://cdn.tutorialzine.com/wp-content/uploads/2013/06/digital_clock-100x100.jpg"
-            },
-            {
-                "url": "http://tutorialzine.com/2013/05/diagonal-fade-gallery/",
-                "title": "Smooth Diagonal Fade Gallery with CSS3 Transitions",
-                "image": "http://cdn.tutorialzine.com/wp-content/uploads/2013/05/featured-100x100.jpg"
-            },
-            {
-                "url": "http://tutorialzine.com/2013/05/mini-ajax-file-upload-form/",
-                "title": "Mini AJAX File Upload Form",
-                "image": "http://cdn.tutorialzine.com/wp-content/uploads/2013/05/ajax-file-upload-form-100x100.jpg"
-            },
-            {
-                "url": "http://tutorialzine.com/2013/04/services-chooser-backbone-js/",
-                "title": "Your First Backbone.js App – Service Chooser",
-                "image": "http://cdn.tutorialzine.com/wp-content/uploads/2013/04/service_chooser_form-100x100.jpg"
-            }
-        ];
-
     this.includedItems = [];
-    //Tableau des produits à inclure
+    this.dispoItems = [];
+    $http.get("./app/dispoItems.json").then(
+        function (rep) {
+            self.dispoItems = rep.data;
+        }
+    );
 
+    this.selectItems = function () {
+        self.includedItems.push(this.selectedDispoItems[0]);
+        nb = self.dispoItems.indexOf(this.selectedDispoItems[0]);
+        self.dispoItems.splice(nb, 1);
+        //Je supprime à l'index nb 1 char
+    };
 
-    this.selectedDispoItems = [];
-    //Tableau des éléments sélectionnés dans la liste des produits disponibles
+    this.deselectItems = function () {
 
-    this.selectedIncludedItems = [];
-    //Tableau des éléments sélectionnés dans la liste des produits à inclure
+        self.dispoItems.push(this.selectedDispoItems[0]);
+        nb = self.includedItems.indexOf(this.selectedDispoItems[0]);
+        self.includedItems.splice(nb, 1);
+        //Chemin inverse
+
+    };
+
 
     this.step = 0;
     //Variable d'état agissant sur les vues et prenant les valeurs 1 : choix des produits ou 2 : visualisation du choix
 
     this.addToIncluded = function () {
         //Ajoute les produits disponibles en surbrillance (selectedDispoItems) à la liste des produits à inclure (includedItems)
+
+        this.selectedDispoItems.forEach(function (elem) {
+            self.includedItems.push(elem);
+            nb = self.dispoItems.indexOf(elem);
+            self.dispoItems.splice(nb, 1);
+        })
+
     };
 
-    this.addAllToIncluded = function (){
+    this.addAllToIncluded = function () {
         //Ajoute tous les produits disponibles (dispoItems) à la liste des produits à inclure (includedItems)
 
+        self.dispoItems.forEach(function (elem) {
+            self.includedItems.push(elem);
+        });
+
+        self.dispoItems = [];
+
     };
 
-    this.removeFromIncluded = function(){
+    this.removeFromIncluded = function () {
         //Retire les produits sélectionnés en surbrillance (selectedIncludedItems) pour les remettre dans la liste des produits disponibles (items)
+
+        this.selectedIncludeItems.forEach(function (elem) {
+            self.dispoItems.push(elem);
+            nb = self.includedItems.indexOf(elem);
+            self.includedItems.splice(nb, 1);
+        })
+
     };
 
-    this.RemoveAllFromIncluded = function(){
+    this.RemoveAllFromIncluded = function () {
         //Retire tous les produits à inclure (includedItems) pour les remettre dans la liste des produits disponibles (items)
+
+        this.includedItems.forEach(function (elem) {
+            self.dispoItems.push(elem)
+        });
+        self.includedItems = [];
     }
 
 

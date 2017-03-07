ec68a2f42bfeac148e911ea7d9283f3675b6ce87
Guillaume Gaujac <ggaujacpro@gmail.com>
2017-03-07 15:58:01 +0100

    1er partie de l'exo 1 finis comencement du 2e.

---
+++ td3/ex2/app/CalcApp.js
@@ -0,0 +1,4 @@
+/**
+ * Created by Gaujac on 07/03/2017.
+ */
+var calcApp = angular.module('calcApp',[]);
---
+++ td3/ex2/app/controllers/CalcController.js
@@ -0,0 +1,7 @@
+/**
+ * Created by Gaujac on 07/03/2017.
+ */
+var calcController = calcApp.controller('calcController', function () {
+
+    }
+);
